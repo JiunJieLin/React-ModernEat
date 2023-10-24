5ecfb22b7bba97f307753eab2b66255250e5f7aa
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-24 20:03:36 +0800

    build UI

---
+++ src/components/ExtraAction/index.js
@@ -0,0 +1,17 @@
+import { PlusIcon } from "@radix-ui/react-icons";
+
+const ExtraAction = () => {
+	return (
+		<div className="px-12 flex gap-4">
+			<div className="bg-slate-100 text-gray-850 px-4 text-sm py-2 rounded-3xl cursor-pointer hover:bg-slate-200 transition-opacity flex items-center gap-1">
+				查看類似商品
+			</div>
+			<div className="bg-slate-100 text-gray-850 px-4 text-sm py-2 rounded-3xl cursor-pointer hover:bg-slate-200 transition-opacity flex items-center gap-1">
+				<PlusIcon className="w-4 h-4" />
+				團購訂單
+			</div>
+		</div>
+	);
+};
+
+export default ExtraAction;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import Navigation from "@/components/Navigation";
-
+import Breadcrumb from "@/components/Breadcrumb";
+import Advertisement from "@/components/Advertisement";
+import ShopInfo from "@/components/ShopInfo";
+import ExtraAction from "@/components/ExtraAction";
 const Home = () => {
   return (
     <main className="min-h-screen mx-auto max-w-[1920px]">
       <Navigation />
+      <Breadcrumb />
+      <Advertisement />
+      <ShopInfo />
+      <ExtraAction />
     </main>
   );
 };
