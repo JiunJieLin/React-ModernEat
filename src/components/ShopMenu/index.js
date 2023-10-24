58c02d6cdbd6397f2604d742f48c9b2d6a215d49
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-25 01:38:55 +0800

    UI finish

--- src/components/ShopMenu/index.js
+++ src/components/ShopMenu/index.js
@@ -1,13 +1,15 @@
 import MenuCatecory from "../MenuCategory";
 import CategoryItems from "../CategoryItems";
 const ShopMenu = () => {
-  <div className="flex items-start relative min-h-screen px-14 pt-8">
-    <div className="w=[320px] pr-5 sticky top-0 left-0">
-      <MenuCatecory />
+  return (
+    <div className="flex items-start relative min-h-screen px-14 pt-8">
+      <div className="w-[320px] pr-5 sticky top-0 left-0">
+        <MenuCatecory />
+      </div>
+      <div className="flex-1">
+        <CategoryItems />
+      </div>
     </div>
-    <div className="flex-1">
-      <CategoryItems />
-    </div>
-  </div>;
+  );
 };
 export default ShopMenu;
