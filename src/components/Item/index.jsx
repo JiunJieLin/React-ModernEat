e935ecc0a3553daa54a27639935f5fce3378fe1f
JiunJieLin <sl10066116@gmail.com>
2023-11-05 21:37:56 +0800

    heart click

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,10 +2,17 @@ import AddButton from "../AddButton/AddButton";
 import { useContext } from "react";
 import { ProductContext } from "@/data/context";
 import { useCartActions } from "../hooks/useCartActions";
+import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 const Item = ({ data }) => {
   const { cart, setCart } = useContext(ProductContext);
   const { handleOnAdd, handleOnRemove } = useCartActions(cart, setCart);
+
+  const [clicked, setClicked] = useState(false);
+  const handleHeartClick = () => {
+    setClicked(!clicked);
+  };
 
   const existingCartItem = cart.find((item) => item.id === data.id);
   const itemCount = existingCartItem ? existingCartItem.count : 0;
@@ -20,9 +27,18 @@ const Item = ({ data }) => {
           itemCount={itemCount}
         />
       </div>
-      <div className="py-2 text-stone-950">
+      <div className="py-2 text-stone-950 relative">
         <div>{data.title}</div>
-        <div className="text-sm opacity-70">${data.price}</div>
+        <div className="flex relative">
+          <div className="text-sm opacity-70">${data.price}</div>
+          <button onClick={handleHeartClick} className="absolute top-0 right-3">
+            {clicked ? (
+              <HeartFilledIcon className="text-red-500" />
+            ) : (
+              <HeartIcon className="text-red-500" />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
