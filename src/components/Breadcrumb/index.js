c85c66e67605c2938a111b8ee5708dd6ce2e8320
JiunJieLin <sl10066116@gmail.com>
2023-11-08 01:34:27 +0800

    test

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -7,7 +7,7 @@ const Breadcrumb = () => {
         <ChevronRightIcon className="ml-2" />
       </div>
       <div className="flex items-center mr-2">
-        台北市
+        台中市
         <ChevronRightIcon className="ml-2" />
       </div>
       <div className="flex items-center mr-2">
