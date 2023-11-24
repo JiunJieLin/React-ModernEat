3c718f8a8554d762c964b774e96463f45ffd1d2c
JiunJieLin <sl10066116@gmail.com>
2023-11-24 23:50:37 +0800

    change to items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
-// import Navigation from "@/components/Navigation";
-// import Breadcrumb from "@/components/Breadcrumb";
-// import Advertisement from "@/components/Advertisement";
-// import ShopInfo from "@/components/ShopInfo";
-// import ExtraAction from "@/components/ExtraAction";
-// import ShopMenu from "@/components/ShopMenu";
+import Navigation from "@/components/Navigation";
+import Breadcrumb from "@/components/Breadcrumb";
+import Advertisement from "@/components/Advertisement";
+import ShopInfo from "@/components/ShopInfo";
+import ExtraAction from "@/components/ExtraAction";
+import ShopMenu from "@/components/ShopMenu";
 import ShoppingList from "@/components/ShoppingList/ShoppingList";
 import { ProductProvider } from "@/data/context";
 const Home = () => {
   return (
     <ProductProvider>
       <main className="min-h-screen mx-auto max-w-[1920px]">
-        {/* <Navigation />
+        <Navigation />
         <Breadcrumb />
         <Advertisement />
         <ShopInfo />
         <ExtraAction />
-        <ShopMenu /> */}
-        <ShoppingList />
+        <ShopMenu />
       </main>
     </ProductProvider>
   );
