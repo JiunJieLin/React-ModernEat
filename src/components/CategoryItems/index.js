3419356876802e29c1d6761f103aa18f92f7fed2
JiunJieLin <sl10066116@gmail.com>
2023-11-26 22:41:35 +0800

    scroll bar

--- src/components/CategoryItems/index.js
+++ src/components/CategoryItems/index.js
@@ -1,13 +1,40 @@
+import { forwardRef, useEffect } from "react";
+import map from "lodash/map";
 import Item from "@/components/Item";
-import mockData from "@/data/mockData.json";
 
-const CategoryItems = () => {
+const CategoryItems = forwardRef(function CategoryItems(
+  { data, active, onClick },
+  ref
+) {
+  useEffect(() => {
+    const activeElement = document.querySelector(`[data-name="${active}"]`);
+    activeElement?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }, [active]);
+
   return (
-    <div className="grid grid-cols-4">
-      {mockData.map((item) => (
-        <Item data={item} key={item.id} />
-      ))}
+    <div className="pb-[100px]" ref={ref}>
+      {map(data, (items, title) => {
+        return (
+          <div key={title} data-cat={title}>
+            <div
+              data-name={title}
+              className="px-4 text-xl font-bold text-slate-900 mt-4"
+            >
+              {title}
+            </div>
+            <div className="grid grid-cols-4">
+              {items?.map((item) => {
+                return <Item key={item.id} data={item} onClick={onClick} />;
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-};
+});
+
 export default CategoryItems;
--- src/components/ShopMenu/index.js
+++ src/components/ShopMenu/index.js
@@ -1,15 +1,66 @@
-import MenuCatecory from "../MenuCategory";
-import CategoryItems from "../CategoryItems";
+import { useEffect, useState } from "react";
+import MenuCategory from "@/components/MenuCategory";
+import CategoryItems from "@/components/CategoryItems";
+import mockData from "@/data/mockData";
+
 const ShopMenu = () => {
+  const [activeCategory, setActiveCategory] = useState("");
+  const [category, setCategory] = useState([]);
+  const [items, setItems] = useState([]);
+
+  /**
+   * 透過 useEffect 來取得資料
+   * 處理資料，切記不要直接使用資料，要先處理過後再使用
+   * 我們這邊是要取得分類資料，所以我們要先取得所有的分類，然後再取得分類下的商品資料
+   *
+   * 我們設計兩個 function
+   * 1. 取得分類資料 refinedData
+   * 2. 取得分類下的商品資料 refinedItems
+   */
+  useEffect(() => {
+    // fetch data form api
+    const refinedData = mockData
+      .map((item) => item.category)
+      ?.filter((item, index, array) => array.indexOf(item) === index);
+
+    setCategory(refinedData);
+
+    // order by category
+    const refinedItems = mockData.reduce((acc, cur) => {
+      const category = cur.category;
+      if (!acc[category]) {
+        acc[category] = [];
+      }
+      acc[category].push(cur);
+      return acc;
+    }, {});
+
+    setItems(refinedItems);
+  }, []);
+
+  const handleOnMenuCategoryClick = (index) => {
+    setActiveCategory(index);
+  };
+
+  const activeCategoryLabel = category?.[activeCategory];
+
+  /**
+   * 將處理好的資料傳遞給子元件
+   */
   return (
     <div className="flex items-start relative min-h-screen px-14 pt-8">
       <div className="w-[320px] pr-5 sticky top-0 left-0">
-        <MenuCatecory />
+        <MenuCategory
+          data={category}
+          active={activeCategory}
+          onClick={handleOnMenuCategoryClick}
+        />
       </div>
       <div className="flex-1">
-        <CategoryItems />
+        <CategoryItems data={items} active={activeCategoryLabel} />
       </div>
     </div>
   );
 };
+
 export default ShopMenu;
