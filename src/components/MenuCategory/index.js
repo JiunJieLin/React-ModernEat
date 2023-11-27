45243a3e95d47840416b76b85a00e02007ea9e12
JiunJieLin <sl10066116@gmail.com>
2023-11-27 23:29:42 +0800

    css changes

--- src/components/MenuCategory/index.js
+++ src/components/MenuCategory/index.js
@@ -3,7 +3,7 @@ import MenuItem from "@/components/MenuItem/MenuItem";
 const MenuCategory = ({ data, active, onClick }) => {
   return (
     <div className="max-h-screen overflow-auto no-scrollbar">
-      <div className="pb-20 pt-">
+      <div className="pb-20 ">
         <div className="mb-1 underline font-bold text-stone-950">最熱門</div>
         {data?.map((item, index) => {
           return (
