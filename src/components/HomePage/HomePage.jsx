0d739d210d81ddb5cdf831ac27127b4c49bb2fd5
JiunJieLin <sl10066116@gmail.com>
2023-12-02 15:07:48 +0800

    Flipcard

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,33 +1,38 @@
 import Flipcard from "./Flipcard";
 import Navigation from "@/components/Navigation/Navigation.jsx";
 import PopularStores from "@/components/HomePage/PopularStores";
+import { ThemeContext } from "@/data/context";
+import { useContext } from "react";
 const HomePage = () => {
+  const { darkMode } = useContext(ThemeContext);
   return (
-    <div>
-      <Navigation />
-      <div
-        className="min-h-screen bg-cover bg-center relative"
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
-        }}
-      >
-        <p className=" absolute top-[200px] left-10 text-[30px] sm:text-[40px]">
-          肚子餓了嗎？最多種類手刀下單！
-        </p>
-        <div className=" absolute top-[300px] left-10  text-[20px] flex gap-4  items-end">
-          <button className="px-2 py-3 text-white bg-slate-700 rounded-md hover:opacity-80">
-            尋找食物
-          </button>
-          <p className="font-bold ">
-            或
-            <a href="" className="underline ml-2">
-              登入
-            </a>
+    <div className={`${darkMode && "dark"}`}>
+      <div className="bg-white dark:bg-primary-dark">
+        <Navigation />
+        <div
+          className="min-h-screen bg-cover bg-center relative"
+          style={{
+            backgroundImage: `url("https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
+          }}
+        >
+          <p className=" absolute top-[200px] left-10 text-[30px] sm:text-[40px]">
+            肚子餓了嗎？最多種類手刀下單！
           </p>
+          <div className=" absolute top-[300px] left-10  text-[20px] flex gap-4  items-end">
+            <button className="px-2 py-3 text-white bg-slate-700 rounded-md hover:opacity-80">
+              尋找食物
+            </button>
+            <p className="font-bold ">
+              或
+              <a href="" className="underline ml-2">
+                登入
+              </a>
+            </p>
+          </div>
         </div>
+        <PopularStores />
+        <Flipcard />
       </div>
-      <PopularStores />
-      {/* <Flipcard /> */}
     </div>
   );
 };
