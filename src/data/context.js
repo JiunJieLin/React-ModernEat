8d041afa443dc232171eda1aa59c325def208d4b
JiunJieLin <sl10066116@gmail.com>
2023-12-02 22:38:47 +0800

    localStorage

--- src/data/context.js
+++ src/data/context.js
@@ -1,18 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
 export const ProductContext = createContext({});
 export const ThemeContext = createContext();
+
 export const ProductProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const isLocalStorageAvailable =
+    typeof window !== "undefined" && window.localStorage;
+
+  const initialDarkMode =
+    (isLocalStorageAvailable && localStorage.getItem("darkMode") === "true") ||
+    false;
+  const initialCart =
+    (isLocalStorageAvailable && JSON.parse(localStorage.getItem("cart"))) || [];
+
+  const [cart, setCart] = useState(initialCart);
+  const [darkMode, setDarkMode] = useState(initialDarkMode);
+
+  useEffect(() => {
+    if (isLocalStorageAvailable) {
+      localStorage.setItem("darkMode", darkMode.toString());
+    }
+  }, [darkMode, isLocalStorageAvailable]);
+
+  useEffect(() => {
+    if (isLocalStorageAvailable) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart, isLocalStorageAvailable]);
+
   const toggleDarkMode = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
   };
+
   const contextValue = {
     cart,
     setCart,
     darkMode,
     toggleDarkMode,
   };
+
   return (
     <ProductContext.Provider value={contextValue}>
       <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
