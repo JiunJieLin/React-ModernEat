a7ad535553f3e07fd3f539838b55ed58ef717faa
JiunJieLin <sl10066116@gmail.com>
2023-12-10 20:48:24 +0800

    item style changes

--- src/components/Shop/Item.jsx
+++ src/components/Shop/Item.jsx
@@ -24,7 +24,7 @@ const Item = ({ data }) => {
 
   return (
     <div className="mb-1 p-4 hover:shadow-xl ">
-      <div className="aspect-video overflow-hidden flex items-center relative ">
+      <div className="aspect-video overflow-hidden rounded-t-lg flex items-center relative ">
         <img src={data.image} alt="" />
         <AddButton
           onAdd={(e) => handleOnAdd(data, e)}
@@ -32,7 +32,7 @@ const Item = ({ data }) => {
           itemCount={itemCount}
         />
       </div>
-      <div className="py-2 text-stone-950 relative bg-white dark:bg-slate-300">
+      <div className="py-2 text-stone-950 rounded-b-lg relative bg-white dark:bg-slate-300">
         <div>{data.title}</div>
         <div className="flex relative">
           <div className="text-sm opacity-70">${data.price}</div>
